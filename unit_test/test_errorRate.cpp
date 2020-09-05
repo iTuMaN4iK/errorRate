9dfbdff18d188afba1d2e682845246fd98f177c8
iTuMaN4iK <swaga.ituman4ik@gmail.com>
2020-09-05 21:49:51 +0500

    Отработка после windows на linux

--- unit_test/test_errorRate.cpp
+++ unit_test/test_errorRate.cpp
@@ -5,47 +5,47 @@ TEST(getErrorRateIntervals, eq) {
   {
     Frames last{1000, 1};
     Frames next{2000, 501};
-    EXPECT_FLOAT_EQ(getErrorRateIntervals(last, next), 0.5);
+    EXPECT_DOUBLE_EQ(getErrorRateIntervals(last, next), 0.5);
   }
   {
     Frames last{2000, 501};
     Frames next{3000, 1001};
-    EXPECT_FLOAT_EQ(getErrorRateIntervals(last, next), 0.5);
+    EXPECT_DOUBLE_EQ(getErrorRateIntervals(last, next), 0.5);
   }
   {
     Frames last{3000, 1001};
     Frames next{4000, 1501};
-    EXPECT_FLOAT_EQ(getErrorRateIntervals(last, next), 0.5);
+    EXPECT_DOUBLE_EQ(getErrorRateIntervals(last, next), 0.5);
   }
   {
     Frames last{4000, 1501};
     Frames next{5000, 2001};
-    EXPECT_FLOAT_EQ(getErrorRateIntervals(last, next), 0.5);
+    EXPECT_DOUBLE_EQ(getErrorRateIntervals(last, next), 0.5);
   }
   {
     Frames last{5000, 2001};
     Frames next{25000, 2101};
-    EXPECT_FLOAT_EQ(getErrorRateIntervals(last, next), 0.005);
+    EXPECT_DOUBLE_EQ(getErrorRateIntervals(last, next), 0.005);
   }
   {
     Frames last{25000, 2101};
     Frames next{45000, 2201};
-    EXPECT_FLOAT_EQ(getErrorRateIntervals(last, next), 0.005);
+    EXPECT_DOUBLE_EQ(getErrorRateIntervals(last, next), 0.005);
   }
   {
     Frames last{2000, 501};
     Frames next{3000, 511};
-    EXPECT_FLOAT_EQ(getErrorRateIntervals(last, next), 0.01);
+    EXPECT_DOUBLE_EQ(getErrorRateIntervals(last, next), 0.01);
   }
   {
     Frames last{3000, 511};
     Frames next{4000, 531};
-    EXPECT_FLOAT_EQ(getErrorRateIntervals(last, next), 0.02);
+    EXPECT_DOUBLE_EQ(getErrorRateIntervals(last, next), 0.02);
   }
   {
     Frames last{4000, 531};
     Frames next{5000, 561};
-    EXPECT_FLOAT_EQ(getErrorRateIntervals(last, next), 0.03);
+    EXPECT_DOUBLE_EQ(getErrorRateIntervals(last, next), 0.03);
   }
 }
 
@@ -65,18 +65,18 @@ TEST(getErrorRate, eq) {
     er.setCurrent(4, {5000, 2001});
     er.setCurrent(24, {25000, 2101});
     er.setCurrent(44, {45000, 2201});
-    EXPECT_FLOAT_EQ(er.getErrorRate(44), 0.05);
+    EXPECT_DOUBLE_EQ(er.getErrorRate(44), 0.05);
   }
   {
     ErrorRate er;
     er.setCurrent(0, {1000, 1});
-    EXPECT_FLOAT_EQ(er.getErrorRate(), 0.001);
+    EXPECT_DOUBLE_EQ(er.getErrorRate(), 0.001);
   }
   {
     ErrorRate er;
     er.setCurrent(0, {1000, 1});
-    er.setCurrent(44, {45000, 500});
-    EXPECT_FLOAT_EQ(er.getErrorRate(), 0.001);
+    er.setCurrent(44, {5000, 2500});
+    EXPECT_DOUBLE_EQ(er.getErrorRate(), 0.5);
   }
   {
     ErrorRate er;
@@ -87,7 +87,7 @@ TEST(getErrorRate, eq) {
     er.setCurrent(4, {5000, 2001});
     er.setCurrent(24, {25000, 2101});
     er.setCurrent(44, {45000, 2201});
-    EXPECT_FLOAT_EQ(er.getErrorRate(48), 0.05);
+    EXPECT_DOUBLE_EQ(er.getErrorRate(48), 0.05);
   }
   {
     ErrorRate er;
@@ -96,7 +96,7 @@ TEST(getErrorRate, eq) {
     er.setCurrent(4, {3000, 511});
     er.setCurrent(6, {4000, 531});
     er.setCurrent(8, {5000, 561});
-    EXPECT_FLOAT_EQ(er.getErrorRate(6), 0.02);
+    EXPECT_DOUBLE_EQ(er.getErrorRate(6), 0.02);
   }
   {
     ErrorRate er;
@@ -104,6 +104,6 @@ TEST(getErrorRate, eq) {
     er.setCurrent(4, {3000, 511});
     er.setCurrent(6, {4000, 531});
     er.setCurrent(8, {5000, 561});
-    EXPECT_FLOAT_EQ(er.getErrorRate(6), 0.025);
+    EXPECT_DOUBLE_EQ(er.getErrorRate(6), 0.025);
   }
 }
